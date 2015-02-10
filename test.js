0fccd3b6536ed43505945dc4dbe30433b3dc41af
Nala Ginrut <nalaginrut@gmail.com>
2015-02-10 19:40:14 +0800

    test stuffs

--- test.js
+++ test.js
@@ -2,10 +2,17 @@ function myCallback (data) {
   $('#demo').innerHTML = data.name;
 }
 
+function ajax_callback (data) {
+  $('#demo').innerHTML = data;
+}
+
 function start () {
   //$('#demo').innerHTML = $('#mmr input').length;
 
-  var req = HTTP_Request('jsonp');
-  req.send('http://api.szdiy.org/jsonp/myCallback');
+  //var http = HTTP('jsonp');
+  //http.get('http://api.szdiy.org/jsonp/myCallback');
+
+  var http = HTTP('ajax', ajax_callback);
+  http.get('/test');
 }
 
